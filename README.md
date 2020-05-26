276597828844705f218a3321cf020baab1ffc163
veggiesaurus <accomrie@gmail.com>
2020-05-26 15:35:22 +0200

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
+<<<<<<< HEAD
 This repository contains two deployments systems on deploying interactive applications, using the Cube Analysis and Rendering Tool for Astronomy (CARTA) as the use case. We compare the use of Kubernetes as an orchestration framework and Slurm as a resource manager in HPC environment. The objective is to find the best deployment system for interactive applications. For each deployment method we going to list the scripts that needs to be executed. Below is the technical overview of CARTA deployed on both Kubernetes and on HPC using Slurm as a resource manager. 
 
 TECHNICAL OVERVIEW
 
 ![Uploading architecture-diagram.png…]()
+=======
+# Cookies and JWT
+
+Basic example of using JWTs and returning them via cookies. 
+
+To test: 
+1. Run `npm install`
+2. Copy `config.js.stub` to `config.js` and edit if neccessary
+3. Run using `npm start`
+4. Send a `POST` to `http://localhost:8000/login` with username and password in a JSON body. If the username and password match the dummy values in `config.js`, the server will respond with `{"success": true}`, and a JWT stored as a cookie.
+5. Send a `GET` to `http://localhost:8000/test`. The server will verify the JWT sent to it as a cookie, and return `{"success": true}` if it is valid.
+>>>>>>> ac208d1 (first commit)
---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+// Auth config
+const config = require("./config.js");
+
+let app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+handleLogin = (req, res) => {
+    if (!req.body) {
+        res.status(400).json({
+            success: false,
+            message: "Malformed login request"
+        });
+        return;
+    }
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Dumy auth
+    if (username !== config.dummyUsername || password !== config.dummyPassword) {
+        res.status(403).json({
+            success: false,
+            message: "Invalid username/password combo"
+        });
+        return;
+    } else {
+        const token = jwt.sign({
+            username: username,
+            backendSocket: config.backendSocket 
+        }, config.secret, {expiresIn: '1h'});        
+        res.cookie(config.tokenName, token, {maxAge: 1000 * 60 * 60});
+        res.json({
+            success: true,
+            message: "Successfully authenticated"            
+        });
+    }
+}
+
+handleGet = (req, res) => {    
+    const tokenCookie = req.cookies[config.tokenName];
+    if (tokenCookie) {
+        try {
+            const token = jwt.verify(tokenCookie, config.secret);            
+            res.json({
+                success: true,                
+                message: `Authorised as ${token.username}`
+            });
+            return;
+        } catch(err) {
+            res.json({
+                success: false,
+                message: err
+            });
+            return;
+        }                
+    } else {
+        res.status(403).json({
+            success: false,
+            message: "Not authorized"
+        });
+    }
+}
+
+app.post("/login", handleLogin);
+app.get("/test", handleGet);
+app.listen(port, ()=>console.log(`Started listening for login requests on port ${port}`));
