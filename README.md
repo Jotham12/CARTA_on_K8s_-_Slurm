eade969d603b95f814ca641618d871c8015635e3
veggiesaurus <accomrie@gmail.com>
2020-06-01 22:46:17 +0200

    test of spawning process as user specified by JWT

--- README.md
+++ README.md
@@ -11,8 +11,18 @@ Basic example of using JWTs and returning them via cookies.
 
 To test: 
 1. Run `npm install`
-2. Copy `config.js.stub` to `config.js` and edit if neccessary
+2. Copy `config.ts.stub` to `config.ts` and edit if neccessary
 3. Run using `npm start`
+<<<<<<< HEAD
 4. Send a `POST` to `http://localhost:8000/login` with username and password in a JSON body. If the username and password match the dummy values in `config.js`, the server will respond with `{"success": true}`, and a JWT stored as a cookie.
 5. Send a `GET` to `http://localhost:8000/test`. The server will verify the JWT sent to it as a cookie, and return `{"success": true}` if it is valid.
 >>>>>>> ac208d1 (first commit)
+=======
+4. Send a `POST` to `http://localhost:8000/login` with username and password in a JSON body. If the username and password match the dummy values in `config.ts`, the server will respond with `{"success": true}`, and a JWT stored as a cookie.
+5. Send a `GET` to `http://localhost:8000/checkStatus`. The server will verify the JWT sent to it as a cookie, and return `{"success": true}` if it is valid.
+5. Send a `POST` to `http://localhost:8000/start`. The server will
+    * Verify the JWT sent to it as a cookie.
+    * Kill any existing process spawned for the given user.
+    * Attempt to start the process defined in `config.ts` as the user specified in the JWT
+    * Return `{"success": true}` if spawning succeeds.
+>>>>>>> 1117671 (test of spawning process as user specified by JWT)
---
+++ index.ts
@@ -0,0 +1,132 @@
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
+import * as jwt from "jsonwebtoken";
+import * as fs from "fs";
+import {spawn, ChildProcess} from "child_process";
+
+// Simple type intersection for adding custom username field to an express request
+type AuthenticatedRequest = express.Request & { username?: string, jwt?: string };
+
+// Auth config
+const config = require("./config.ts");
+const publicKey = fs.readFileSync(config.publicKeyLocation);
+
+// Child processes
+const childMap = new Map<string, ChildProcess>();
+
+let app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
+// Optional login route that uses a private key to sign a JWT after authorising
+if (config.handleTokenSigning) {
+    const privateKey = fs.readFileSync(config.privateKeyLocation);
+    const handleLogin = (req, res) => {
+        if (!req.body) {
+            res.status(400).json({success: false, message: "Malformed login request"});
+            return;
+        }
+
+        const username = req.body.username;
+        const password = req.body.password;
+        // Dummy auth
+        if (username !== config.dummyUsername || password !== config.dummyPassword) {
+            res.status(403).json({success: false, message: "Invalid username/password combo"});
+        } else {
+            const token = jwt.sign({
+                username: username,
+                backendSocket: config.backendSocket
+            }, privateKey, {algorithm: config.keyAlgorithm, expiresIn: '1h'});
+            res.cookie(config.tokenName, token, {maxAge: 1000 * 60 * 60});
+            res.json({success: true, message: "Successfully authenticated"});
+        }
+    }
+    app.post("/login", handleLogin);
+} else {
+    app.post("/login", ((req, res) => {
+        res.status(400).json({success: false, message: "Login not implemented"});
+    }))
+}
+
+// This can easily be replaced by another strategy for getting the token from a request
+const getTokenFromCookie = (req: express.Request) => {
+    return req.cookies?.[config.tokenName];
+}
+
+const getTokenFromBody = (req: express.Request) => {
+    return req.body?.token;
+}
+
+const getToken = getTokenFromCookie;
+
+// Express middleware to guard against unauthorized access. Writes the username and jwt to the request object
+const authGuard = (req: AuthenticatedRequest, res: express.Response, next: express.NextFunction) => {
+    const tokenCookie = getToken(req);
+    if (tokenCookie) {
+        try {
+            const token = jwt.verify(tokenCookie, publicKey, {algorithm: config.keyAlgorithm});
+            req.username = token.username;
+            req.jwt = tokenCookie;
+            next();
+        } catch (err) {
+            res.json({success: false, message: err});
+        }
+    } else {
+        res.status(403).json({success: false, message: "Not authorized"});
+    }
+}
+
+const handleStatus = (req: AuthenticatedRequest, res: express.Response) => {
+    res.json({
+        success: true,
+        username: req.username,
+    });
+}
+
+const handleStart = (req: AuthenticatedRequest, res: express.Response) => {
+    if (!req.username) {
+        res.status(400).json({success: false, message: "Invalid username"});
+    }
+
+    // Kill existing backend process for this
+    try {
+        const currentChild = childMap.get(req.username);
+        if (currentChild) {
+            currentChild.removeAllListeners();
+            currentChild.kill();
+            console.log(`Killed existing process ${currentChild.pid}`);
+            childMap.delete(req.username);
+        }
+    } catch (e) {
+        console.log(`Error killing existing process belonging to user ${req.username}`);
+        res.status(400).json({success: false, message: "Problem killing existing process"});
+        return;
+    }
+
+    // Spawn a new process
+    try {
+        const child = spawn("sudo", ["-u", `${req.username}`, config.processCommand]);
+        child.stdout.on("data", data => console.log(data.toString()));
+        child.on('exit', (code, signal) => {
+            console.log(`Exited via signal ${signal} and code ${code}`);
+        });
+        console.log(`Started process with PID ${child.pid} for user ${req.username}`);
+        child.pid
+        childMap.set(req.username, child);
+        res.json({success: true, username: req.username, token: req.jwt});
+    } catch (e) {
+        console.log(`Error killing existing process belonging to user ${req.username}`);
+        res.status(400).json({success: false, message: `Problem starting process for user ${req.username}`});
+        return;
+    }
+}
+
+app.post("/start", authGuard, handleStart);
+app.get("/checkStatus", authGuard, handleStatus);
+
+app.listen(port, () => console.log(`Started listening for login requests on port ${port}`));
